f0d404ce0f421cee93bb9d10548314c8b442cf05
satheeshkumarv2005 <166595540+satheeshkumarv2005@users.noreply.github.com>
2024-06-29 11:13:22 +0530

    Update Rock-Paper-Scissor -game.py

--- Rock-Paper-Scissor -game.py
+++ Rock-Paper-Scissor -game.py
@@ -1,1 +1,30 @@
+import random
 
+user_choice = input('enter your choice(rock/paper/scissor)')
+print(f'user choosed as {user_choice}')
+computer_choice = random.choice(['rock','paper','scissor'])
+print(f'computer choosed as {computer_choice}')
+
+if user_choice == computer_choice:
+    print('game tied')
+else:
+    if user_choice == 'scissor':
+        if computer_choice == 'paper':
+            print('user won the game')
+        else:
+            print('computer won the game')
+
+    elif user_choice == 'paper':
+        if computer_choice == 'rock':
+            print('user won the game')
+        else:
+            print('computer won the game')
+
+    elif user_choice == 'rock':
+        if computer_choice == 'scissor':
+            print('user won the game')
+        else:
+            print('computer won the game')
+
+    else:
+        print('invalid choice.enter the proper choice to continue the game')
